62ef71f6d05604b140acf118b6409b96cdee8f1d
kangtaewan <bras1717@naver.com>
2023-01-25 23:05:14 +0900

    유튜브 api 셋팅

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { useParams } from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
-import { search } from "../api/youtube";
-import FakeYoutube from "../api/fakeYoutube";
+import Youtube from "../api/youtube";
 export default function Videos() {
 	const { keyword } = useParams();
 	const { isLoading, error, data: videos} = useQuery(
 		["videos", keyword], () => {
-			const youtube = new FakeYoutube();
+			const youtube = new Youtube();
 			return youtube.search(keyword);
 		});
 	return (
@@ -17,9 +16,7 @@ export default function Videos() {
 			{isLoading && <p>Loading...</p>}
 			{error && <p>Something is wrong</p>}
 			{videos && <ul>
-				{
-					videos.map(video => <VideoCard key={video.id} video={video}/>)
-				}
+				{videos.map(video => <VideoCard key={video.id} video={video}/>)}
 			</ul>}
 		</>
 	);
