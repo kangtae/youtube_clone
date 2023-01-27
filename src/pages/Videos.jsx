327364a45b786f20862020543ad9af0eba9e5b0c
kangtaewan <bras1717@naver.com>
2023-01-27 22:55:27 +0900

    유튜브 context 셋팅

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,13 +3,13 @@ import { useParams } from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
 import Youtube from "../api/youtube";
+import {useYoutubeApi} from "../context/YoutubeApiContext";
 export default function Videos() {
 	const { keyword } = useParams();
+	const { youtube } = useYoutubeApi();
 	const { isLoading, error, data: videos} = useQuery(
-		["videos", keyword], () => {
-			const youtube = new Youtube();
-			return youtube.search(keyword);
-		});
+		["videos", keyword], () =>
+			youtube.search(keyword));
 	return (
 		<>
 			<div>Videos {keyword ? `🖥${keyword}`: "❤️"}</div>
